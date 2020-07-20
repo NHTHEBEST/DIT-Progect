73a1922fedc21061665be3149a0a491e47ed8908
Noam Hadar <nhthebest.noam.hadar@gmail.com>
2020-07-21 10:34:49 +1200

    comments

--- js/page.js
+++ js/page.js
@@ -1,14 +1,13 @@
-function ReplaceContent(url, name) {
+function ReplaceContent(url, name) {// declar function
 
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function () {
-        if (this.readyState === 4 && this.status === 200) {
+    var xhttp = new XMLHttpRequest();//start web request
+    xhttp.onreadystatechange = function () { // when request complete
+        if (this.readyState === 4 && this.status === 200) { // check if request is ok
             // Typical action to be performed when the document is ready:
-            //document.getElementsByName("demo").innerHTML = xhttp.responseText;
-            document.getElementById("main").innerHTML = xhttp.responseText;
-            window.history.pushState(name, document.title, url);
+            document.getElementById("main").innerHTML = xhttp.responseText; // replace content
+            window.history.pushState(name, document.title, url); // change url
 
-            if (last == null) {
+            if (last == null) { // update menubar
                 last = document.getElementById(name);
             } else {
                 last.classList.remove("active");
@@ -17,7 +16,7 @@ function ReplaceContent(url, name) {
             last.classList.add("active");
         }
     };
-    xhttp.open("GET", "/content/" + url, true);
+    xhttp.open("GET", "/content/" + url, true); // peform request
     xhttp.send();
 
 }
